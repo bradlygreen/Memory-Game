58cb1b35bcc620b31d15113febf34c1211e32b36
Igor Minar <iiminar@gmail.com>
2012-05-08 00:33:44 -0700

    initial cleanup

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,25 +3,22 @@
 
 function Tile(title) {
     this.title = title;
-    this.display = 'back';
     this.flipped = false;
 
-    this.show = function() {
-        this.display = this.title;
-        this.flipped = true;
-    };
+    this.imgName = function() {
+        return this.flipped ?  this.title : 'back';
+    }
 
-    this.hide = function() {
-        this.display = 'back';
-        this.flipped = false;
-    };
+    this.flip = function() {
+      this.flipped = !this.flipped;
+    }
 }
 
 function GridCtrl($scope) {
     $scope.grid = newGame();
     $scope.guesses = [];
 
-    function getTile(tileDeck) {
+    function getRandomTile(tileDeck) {
         var i = Math.floor(Math.random()*tileDeck.length);
         return tileDeck.splice(i, 1)[0];
     }
@@ -29,41 +26,28 @@ function GridCtrl($scope) {
     /* Create an array with two of each tileName in it */
     function makeDeck(tileNames) {
         var tileDeck = [];
-        for (var i = 0; i < tileNames.length; i++) {
-            tileDeck.push(new Tile(tileNames[i]));
-            tileDeck.push(new Tile(tileNames[i]));
-        }
+        tileNames.forEach(function(name) {
+            tileDeck.push(new Tile(name));
+            tileDeck.push(new Tile(name));
+        });
 
         return tileDeck;
     }
 
-    /* Make a 2-D array with the specified number of rows and columns */
-    function makeGrid(numRows, numCols) {
-        var grid = new Array(numRows);
-
-        for (var row = 0; row < numRows; row++) {
-            for (var col = 0; col < numCols; col++) {
-                grid[row] = new Array(numCols);
-            }
-        }
-
-        return grid;
-    }
-
     /* Fill the grid with tiles picked at random */
     function newGame() {
-        //var tileNames = ['foo', 'bar', 'baz', 'bon', 'fir', 'fur', 'onz', 'huq'];
         var tileNames = ['8-ball', 'kronos', 'baked-potato', 'dinosaur', 'rocket', 'skinny-unicorn', 'that-guy', 'zeppelin'];
         var tileDeck = makeDeck(tileNames);
-        var grid = makeGrid(4, 4);
+        var grid = [];
+        var gridDimension = Math.sqrt(tileNames.length);
 
         $scope.message = 'Click on a tile.';
         $scope.unmatchedPairs = tileNames.length;
 
-        for (var row = 0; row < grid.length; row++) {
-            for (var col = 0; col < grid[row].length; col++) {
-                var tile = getTile(tileDeck);
-                grid[row][col] = tile;
+        for (var row = 0; row < gridDimension; row++) {
+            grid[row] = [];
+            for (var col = 0; col < gridDimension; col++) {
+                grid[row][col] = getRandomTile(tileDeck);
             }
         }
 
@@ -77,8 +61,8 @@ function GridCtrl($scope) {
 
         if ($scope.guesses.length === 2) {
             if ($scope.guesses[0].title !== $scope.guesses[1].title) {
-                $scope.guesses[0].hide();
-                $scope.guesses[1].hide();
+                $scope.guesses[0].flip();
+                $scope.guesses[1].flip();
             }
             $scope.guesses = [];
             $scope.message = '';
@@ -86,10 +70,10 @@ function GridCtrl($scope) {
 
         if ($scope.guesses.length === 0) {
             $scope.guesses[0] = tile;
-            tile.show();
+            tile.flip();
         }
         else if (tile !== $scope.guesses[0]) {
-            tile.show();
+            tile.flip();
             $scope.guesses[1] = tile;
 
             if (tile.title === $scope.guesses[0].title) {
